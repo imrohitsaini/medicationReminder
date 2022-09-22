f3c25a83ca7151c0259afc88dceecb1809cfe643
imrohitsaini <rohitsaini999.rs@gmail.com>
2022-09-22 13:03:00 +0530

    Night Mode settings added

--- app/src/main/java/com/oursdevelopers/medicationreminder/MainActivity.kt
+++ app/src/main/java/com/oursdevelopers/medicationreminder/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.oursdevelopers.medicationreminder
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.example.medicationreminder.R
-import com.example.medicationreminder.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatDelegate
+import com.oursdevelopers.medicationreminder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,15 +16,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        binding.btnNightmode.setOnClickListener {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            Toast.makeText(this, "Night Mode", Toast.LENGTH_SHORT).show()
+        }
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-        ))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        binding.btnLightmode.setOnClickListener {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            Toast.makeText(this, "Light Mode", Toast.LENGTH_SHORT).show()
+        }
     }
 }
