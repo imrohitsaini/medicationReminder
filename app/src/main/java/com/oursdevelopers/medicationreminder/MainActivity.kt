cde6b3e495240a8e9554e661c102bf3a322f1e04
imrohitsaini <rohitsaini999.rs@gmail.com>
2022-09-22 17:05:28 +0530

    Fragments Added

--- app/src/main/java/com/oursdevelopers/medicationreminder/MainActivity.kt
+++ app/src/main/java/com/oursdevelopers/medicationreminder/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.oursdevelopers.medicationreminder
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
 import com.oursdevelopers.medicationreminder.databinding.ActivityMainBinding
+import com.oursdevelopers.medicationreminder.ui.HomeFragment
+import com.oursdevelopers.medicationreminder.ui.SettingsFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,17 +15,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnNightmode.setOnClickListener {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-            Toast.makeText(this, "Night Mode", Toast.LENGTH_SHORT).show()
+        loadFragment(HomeFragment())
+
+        binding.bottomNav.setOnItemSelectedListener { item ->
+
+            when (item.itemId) {
+                R.id.item_home -> loadFragment(HomeFragment())
+                R.id.item_settings -> loadFragment(SettingsFragment())
+            }
+
+            return@setOnItemSelectedListener true
         }
 
-        binding.btnLightmode.setOnClickListener {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-            Toast.makeText(this, "Light Mode", Toast.LENGTH_SHORT).show()
-        }
+
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fl_container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
